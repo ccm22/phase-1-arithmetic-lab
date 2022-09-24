297b20305871bcbd7683a41e76c43f636246dea5
Chris Masters <ccm22_@outlook.com>
2022-09-24 13:55:06 +1000

    commit tree 2f347b103ec5e668ca36f8765770e7b8d16c851e

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
 
-// Write your code here
+// Create a variable called random that will generate a random integer greater than 0.
+
+let random = parseInt(Math.random()*100);
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+let num3 = 32;
+let num4 = 8;
+let mod = num3 / num4;
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+let max = Math.max(1, 5, 10, 3, 8, 20);
